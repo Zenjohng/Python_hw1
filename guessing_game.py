1a36e48280450dc925e0468bb2cc4c8487dee61c
Znaid04 <Znaidhr@gmail.com>
2020-08-12 11:10:38 -0400

    prompted user to enter name & guess

---
+++ guessing_game.py
@@ -0,0 +1,12 @@
+# Number Guessing  Game
+
+#assign secrectNumber to random number from range 1-10, inclusive of 10
+secrectNumber = random.randint (1, 11)
+#initialize number or guesses to 1 and call it guess
+numGuesses = 1
+
+name = int (input("Enter your name: ))
+print("name")
+guess = int(input("Enter a guess:"))
+print("guess")
+

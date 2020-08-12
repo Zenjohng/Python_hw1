3b9841e6054c10ef3d1d0fd7bd227f127f168944
Znaid04 <Znaidhr@gmail.com>
2020-08-12 11:20:43 -0400

    added the import random functionality

--- guessing_game.py
+++ guessing_game.py
@@ -1,12 +1,17 @@
 # Number Guessing  Game
-
+import random
 #assign secrectNumber to random number from range 1-10, inclusive of 10
 secrectNumber = random.randint (1, 11)
 #initialize number or guesses to 1 and call it guess
 numGuesses = 1
-
+#prompt user to enter their name and enter their guess
 name = int (input("Enter your name: ))
-print("name")
+   print("name")
 guess = int(input("Enter a guess:"))
-print("guess")
+   print("guess")
+
+   
+
+
+
 

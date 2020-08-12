d90d18db4fab8457fa93eb687b725167ff59800f
Znaid04 <Znaidhr@gmail.com>
2020-08-12 12:06:42 -0400

    started a while loop

--- guessing_game.py
+++ guessing_game.py
@@ -10,6 +10,13 @@ name = int (input("Enter your name: ))
 guess = int(input("Enter a guess:"))
    print("guess")
 
+#create a while loop thats exits when the guess is equal to the secrect number
+#guess the the secrectNumber if guess is > than secrect number the user is will recieve an alert that the number is less than guess
+# if guess is < than secrect number the user is will recieve an alert the number isgreater than guess
+
+while (guess != secrectNumber):
+    if guess > secrectNumber:
+
    
 
 
